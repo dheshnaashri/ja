c6fbe6b2c7bb5d55513f3c34485f922b1851c805
dheshnaashri <dheshnaashri@gmail.com>
2026-06-22 14:31:18 +0530

    factory

---
+++ src/day14_factory.java
@@ -0,0 +1,32 @@
+public class day14_factory {
+    interface Notification{
+        void send (String to , String message );
+    }
+    class EmailNotification implements Notification{
+        @Override
+        public void send(String to, String message) {
+            System.out.println("Sending Enail: "+message+", to "+to);
+        }
+    }
+    class WhatsappNotification implements Notification{
+        @Override
+        public void send(String to, String message) {
+            System.out.println("Sending Whatsapp: "+message +", to "+to);
+        }
+    }
+    static Notification getNotification(day14_factory factory, String type){
+        return switch (type){
+            case "Email"-> factory.new EmailNotification();
+            case "Whatsapp"-> factory.new WhatsappNotification();
+            default -> throw new IllegalArgumentException("Invalid notification type: "+type);
+        };
+    }
+
+    static void main() {
+        day14_factory factory = new day14_factory();
+        String type = "Whatsapp";
+        getNotification(factory, type).send("8756950256","Hi dheshh");
+    }
+}
+
+
